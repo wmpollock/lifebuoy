e36d3f7cbf27ee2d1362bcfdd16d67423cacc942
William Pollock <wmpollock@gmail.com>
2020-09-19 15:48:09 -0700

    Strange that windows is kinder here...

--- bash/bashrc-linux.sh
+++ bash/bashrc-linux.sh
@@ -4,6 +4,9 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
+alias py="python3"
+alias pip="pip3"
+
 export PS1="\[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
 
 # MAS DRACULA
